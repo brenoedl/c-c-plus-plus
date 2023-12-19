2748286ccfe9b0d146e9fbf42d7b4894bb96b85b
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-19 12:37:25 -0300

    Mostrando Coisas

--- c/MostrandoCoisas/MostrandoCoisas.c
+++ c/MostrandoCoisas/MostrandoCoisas.c
@@ -1,1 +1,8 @@
 #include <stdio.h>
+#include <locale.h>
+void main() {
+    setlocale(LC_ALL, "Portuguese");
+    printf("Oi\n\n");
+    printf("Oi. \ntudo \nbem?\n\n");
+    printf("C é \n\"SUPER\"\nfácil\a\n\n");
+}
